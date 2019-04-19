9589a8812e5c3108b0b90e6f731b08dab9868a77
Chentongyong <1830442502@qq.com>
2019-04-19 10:32:00 +0800

    更新项目1

--- htxcx/components/header/header.js
+++ htxcx/components/header/header.js
@@ -1,18 +1,23 @@
 var publics = require('../../public/public.js');
 Component({
-  properties: {//接收传值，是字符串类型
+  properties: { //接收传值，是字符串类型
     paramAtoB: Array,
-    para:Object
+    para: Object
   },
   data: {
     // 弹窗显示控制
     isShow: false,
-    sum:[],
-    sums:[],
-    you:false
+    sum: [],
+    sums: [],
+    you: false,
+    cz: ['我的', '你的', '他的', '她的'],
+    // yous: [],
+    yous: '广州市'
+
   },
-  attached: function () {
-    if (this.properties.para[0].id==1){
+  attached: function() {
+    var that = this;
+    if (this.properties.para[0].id == 1) {
       this.setData({
         you: true
       })
@@ -21,15 +26,50 @@ Component({
       sum: this.properties.paramAtoB,
       sums: this.properties.para
     })
-    publics.funs();
+    // publics.funs();//调用public.js公用js的值
+    console.log(publics.funs())
+
+    wx.request({
+      url: publics.ttps(), //仅为示例，并非真实的接口地址
+      data: {
+        'store.city': '广州市'
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function(res) {
+        // console.log(res.data)
+        var sumst = [];
+        for (var j = 0; j < res.data.length; j++) {
+          sumst.push(res.data[j].text);
+        }
+        that.setData({
+          cz: sumst
+        })
+      },
+      fail: function() {
+
+      }
+    })
+
   },
-  moved: function () { 
+  moved: function() {
     console.log(2222)
   },
-  detached: function () { 
+  detached: function() {
     console.log(3333)
   },
   methods: {
-
+    cancelBut: function(e) { // 传递给父组件 
+      var that = this;
+      this.setData({
+        xz: e.target.dataset.text
+      })
+      var myEventDetail = {
+        pickerShow: e.target.dataset.text,
+        type: 'cancel'
+      } // detail对象，提供给事件监听函数 
+      this.triggerEvent('myevent', myEventDetail) //myevent自定义名称事件，父组件中使用 
+    }
   }
 })
--- htxcx/public/public.js
+++ htxcx/public/public.js
@@ -1,11 +1,36 @@
-function funs(){
-  wx.getStorage({
-    key: 'gzmd',
-    success: function (res) {
-      console.log(res)
-    }
+var st = {}
+wx.getStorage({
+  key: 'gzmd',
+  success: function(res) {
+    st['userName'] = res.data.userName;
+    st['userImg'] = res.data.userImg;
+    st['member'] = res.data.member;
+    st['openid'] = res.data.openid;
+    st['userUid'] = res.data.userUid;
+  }
+})
+
+const requestPromise = function (myUrl,data) {
+  // 返回一个Promise实例对象
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: myUrl,
+      success: res => resolve(res)
+    })
   })
 }
+
+const funs = function() {
+  return st
+}
+
+const ttps = function () {
+  var ss = 'https://wwwgezimd.com/Store/cityaction!main.action';
+  return ss
+}
+
 module.exports = {
-  funs: funs
+  funs: funs,
+  ttps: ttps,
+  requestPromise: requestPromise
 }
